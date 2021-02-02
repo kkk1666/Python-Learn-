317745f05342e44ec88b38bfb9c62127d53e4923
unknown <13535093327@163.com>
2021-02-02 20:49:44 +0800

    firstcommit

---
+++ demo1/demo1.py
@@ -0,0 +1,11 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2021/2/1 16:43
+#@Author : XJH
+#@File : demo1.py
+#@Software:PyCharm
+
+print("hello,world")
+
+'''
+qwert
+'''
---
+++ demo1/demo2.py
@@ -0,0 +1,49 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2021/2/1 16:43
+#@Author : XJH
+#@File : demo1.py
+#@Software:PyCharm
+
+'''
+print("标准化输出")
+a=10
+print("a=",a)
+'''
+
+'''
+age=18
+print("我的年纪是：%d岁"%age);
+print("我的名字是：%s，我的国籍是：%s"%("校长","中国"));
+print("aaa","bbb","ccc")
+print("www","baidu","com",sep=".")
+print("hello",end="")
+print("world",end="\t")
+print("python",end="\n")
+print("end")
+print("end")
+'''
+
+'''
+password=input("请输入密码:\n")
+print("您刚刚输入的密码是:",password)
+'''
+
+'''
+a=10.123
+print(type(a))
+'''
+
+# a=input("输入：\n")
+# print(type(a))
+#
+# print("输入了一个数字：%s" %a)
+
+
+a=int("123")
+print(type(a))
+b=a+200
+print(b)
+
+
+
+
