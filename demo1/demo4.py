8489d40857dab60925432d13066b6fbb9180c265
unknown <13535093327@163.com>
2021-02-04 11:38:07 +0800

    demo4

--- demo1/demo4.py
+++ demo1/demo4.py
@@ -1,4 +1,4 @@
-a#-*- codeing = utf-8 -*-
+#-*- codeing = utf-8 -*-
 #@Time : 2021/2/3 16:53
 #@Author : XJH
 #@File : demo4.py
@@ -17,6 +17,29 @@ a#-*- codeing = utf-8 -*-
 # for x in name:
 #     print(x,end="\t")
 
-a=["aa","bb","cc","dd"]
-for i in range(len(a)):
-    print(i,a[i])
+# a=["aa","bb","cc","dd"]
+# for i in range(len(a)):
+#     print(i,a[i])
+
+
+# i=0
+# while i<5:
+#     print("当前是第%d次执行循环"%(i+1))
+#     print("i=%d"%i)
+#     i+=1
+
+# i=0
+# sum=0
+# while i<100:
+#     i=i+1
+#     sum=sum+i
+#
+# print("1-100求和的sum=%d"%sum)
+
+i=0
+while i<10:
+    i=i+1
+    print("-"*30)
+    if i==5:
+        continue  #结束本次循环
+    print(i)
